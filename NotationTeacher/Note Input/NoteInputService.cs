dd2967671c212ee8be386775ef6abe2ddb70a855
xavierevans23 <43934715+xavierevans23@users.noreply.github.com>
2022-12-27 23:38:09 +0000

    Added basic midi connnectivity.

---
+++ NotationTeacher/Note Input/NoteInputService.cs
@@ -0,0 +1,41 @@
+﻿using NotesAndScales;
+
+namespace NotationTeacher
+{
+    public class NoteInputService
+    {
+        private readonly List<Note> notes;
+
+        public IReadOnlyList<Note> Notes
+        {
+            get
+            {
+                return notes;
+            }
+        }
+
+        public NoteInputService()
+        {
+            notes = new();
+        }
+
+        public event EventHandler? NoteUpdate;
+
+        public void OnNoteUpdate()
+        {
+            NoteUpdate?.Invoke(this, new());
+        }
+
+        public void NoteDown(Note note)
+        {
+            notes.Add(note);
+            Console.WriteLine(note);
+            OnNoteUpdate();
+        }
+
+        public void Clear()
+        {
+            notes.Clear();
+        }
+    }
+}
---
+++ NotationTeacher/wwwroot/Midi.js
@@ -0,0 +1,67 @@
+﻿function setupMidi() {
+    try {        
+        navigator.requestMIDIAccess()
+            .then(onMIDISuccess, onMIDIFailure);        
+    }
+    catch {
+        DotNet.invokeMethodAsync('NotationTeacher', 'ReceiveJavascriptError');
+    }
+}
+
+function onMIDISuccess(midiAccess) {
+
+    DotNet.invokeMethodAsync('NotationTeacher', 'ReceiveMidiAccess');
+
+    midiAccessFound = true;
+    midiAccessObject = midiAccess;
+
+    var foundDevice = false;
+    var deviceName = "No device found.";
+
+    for (var input of midiAccess.inputs.values()) {
+        input.onmidimessage = getMIDIMessage;
+        deviceName = input.name;
+        foundDevice = true;
+    }
+
+    if (foundDevice) {
+        DotNet.invokeMethodAsync('NotationTeacher', 'ReceiveConnectionSuccessful', deviceName);
+    }
+    else {
+        DotNet.invokeMethodAsync('NotationTeacher', 'ReceiveNoMidiDevices');
+    }
+}
+
+var midiAccessFound = false;
+var midiAccessObject = null;
+
+function clearMidiListeners() {
+    if (midiAccessFound) {
+        for (var input of midiAccessObject.inputs.values()) {
+            input.onmidimessage = null;
+        }
+    }
+}
+
+function getMIDIMessage(message) {
+    var command = message.data[0];
+    var note = message.data[1];
+    var velocity = (message.data.length > 2) ? message.data[2] : 0; // a velocity value might not be included with a noteOff command
+
+    switch (command) {
+        case 144: // noteOn
+            if (velocity > 0) {
+                DotNet.invokeMethodAsync('NotationTeacher', 'ReceiveNoteOn', String(note))
+            } else {
+                DotNet.invokeMethodAsync('NotationTeacher', 'ReceiveNoteOff', String(note))
+            }
+            break;
+        case 128: // noteOff
+            DotNet.invokeMethodAsync('NotationTeacher', 'ReceiveNoteOff', String(note))
+            break;        
+    }    
+}
+
+function onMIDIFailure() {
+    DotNet.invokeMethodAsync('NotationTeacher', 'ReceiveNoMidiSupport')
+}
