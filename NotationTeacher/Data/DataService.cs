e4262e219f2099cc2a38db06f24163414309ce92
xavierevans23 <43934715+xavierevans23@users.noreply.github.com>
2022-12-28 01:37:52 +0000

    Added loading and saving data from browser.

--- NotationTeacher/Data/DataService.cs
+++ NotationTeacher/Data/DataService.cs
@@ -1,14 +1,40 @@
-﻿namespace NotationTeacher
+﻿using Blazored.LocalStorage;
+
+namespace NotationTeacher
 {
     public class DataService
     {
         public DataHolder DataHolder {get; private set;}
 
+        private readonly ILocalStorageService localStorage;
         public bool LoadedFromStorage { get; private set; } = false;        
 
-        public DataService()
+        public DataService(ILocalStorageService storage)
         {
+            localStorage = storage;
             DataHolder = new();
+        }
+
+        public async Task LoadData()
+        {
+            string data = await localStorage.GetItemAsync<string>("XmlData");
+            try
+            {
+                DataHolder = DataHolder.FromXml(data);
+                LoadedFromStorage = true;
+            }
+            catch (Exception)
+            {
+                LoadedFromStorage = false;
+            }
+        }
+
+        public async Task SaveData()
+        {
+            if (DataHolder is not null)
+            {
+                await localStorage.SetItemAsync("XmlData", DataHolder.ToXml());
+            }
         }
     }
 }
--- NotationTeacher/Program.cs
+++ NotationTeacher/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using NotationTeacher;
+using Blazored.LocalStorage;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -10,5 +11,7 @@ builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.
 
 builder.Services.AddSingleton<NoteInputService>();
 builder.Services.AddSingleton<DataService>();
+
+builder.Services.AddBlazoredLocalStorageAsSingleton();
 
 await builder.Build().RunAsync();
